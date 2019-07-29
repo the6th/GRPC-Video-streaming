ad59c4d3630f2e4b976c4b06e0502f02215e6a79
the6th <hayashi.tomoki@outlook.com>
2019-07-29 10:24:19 +0900

    check play move on Ubuntu 18,4 + ffmpeg

--- imgTest_client.py
+++ imgTest_client.py
@@ -5,15 +5,18 @@ import cv2
 import imageTest_pb2
 import imageTest_pb2_grpc
 import skvideo.io
+import skvideo.datasets
 
-URL = "/home/nirvan/Desktop/projName/db/DVR_ch7_main_20171015180000_20171015190000.mp4"
+URL = "./Resouces/movie.mp4"
 
 def run():
-  channel = grpc.insecure_channel('192.168.1.195:50051')
-  stub = imageTest_pb2_grpc.ImageTestStub(channel)
-  #temp = cv2.imread('/home/nirvan/img_one.png')
-  for response in stub.Analyse( generateRequests() ):
-      print(str(response.reply))
+    TestPlayMovie()
+    # TestImageShow()
+    # channel = grpc.insecure_channel('127.0.0.1:8080')
+    # stub = imageTest_pb2_grpc.ImageTestStub(channel)
+    # #temp = cv2.imread('/home/nirvan/img_one.png')
+    # for response in stub.Analyse( generateRequests() ):
+    #     print(str(response.reply))
 
 
 def generateRequests():
@@ -31,7 +34,25 @@ def generateRequests():
         frame = cv2.cvtColor( frame, cv2.COLOR_RGB2GRAY )
         frame = bytes(frame)
         yield imageTest_pb2.MsgRequest(img= frame)
-    
+
+def TestImageShow():
+    img = cv2.imread("./Resouces/image.png")
+    cv2.imshow('image',img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def TestPlayMovie():
+
+    videodata = skvideo.io.vread(skvideo.datasets.bigbuckbunny())
+    print(videodata.shape)
+
+    for frame in videodata:
+        print(frame.shape)
+        cv2.imshow('image',frame)
+        key = cv2.waitKey(33)#pauses for 3 seconds before fetching next image
+        if key == 27:#if ESC is pressed, exit loop
+            cv2.destroyAllWindows()
+            break
 
 if __name__ == '__main__':
   run()
